fb4fadb817be8fe1e5dfbcea8f25c5c358752ee3
May Santos <57489512+maveloper@users.noreply.github.com>
2020-10-18 05:05:43 -0300

    Update i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,7 +2,7 @@ const i18n = {
   en: {
     regionLabel: 'united states (english)',
     changeRegionBTN: 'change region',
-    bio: `Hello, I'm a very curious person and passionate about creations, interactivity on multiplatforms and solve problems. I'm enthusiastic about technologies, clean codes, DevOps, behavioral methodology and photography. Currently I'm focused on JS, NodeJS, ReactJS and React Native development, but I'm open to learning everything that is needed to improve myself and help to build a better product.
+    bio: ` interactivity on multiplatforms and solve problems. I'm enthusiastic about technologies, clean codes, DevOps, behavioral methodology and photography. Currently I'm focused on JS, NodeJS, ReactJS and React Native development, but I'm open to learning everything that is needed to improve myself and help to build a better product.
     Some of my soft Skills : Communicative, cognitively flexible, multifocal, creative, organized and analytical.`
   },
   pt_br: {
